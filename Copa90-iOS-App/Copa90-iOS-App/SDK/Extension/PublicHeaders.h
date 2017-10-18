f14795d1c6a07097c1ac5ecf79da0a5765502629
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-10-18 13:17:47 +0330

    Add Public Headers Definitions

---
+++ Copa90-iOS-App/Copa90-iOS-App/SDK/Extension/PublicHeaders.h
@@ -0,0 +1,15 @@
+//
+//  PublicHeaders.h
+//  Copa90-iOS-App
+//
+//  Created by Alirëza WJ Arabi on 10/17/17.
+//  Copyright © 2017 Alirëza WJ Arabi. All rights reserved.
+//
+
+#ifndef PublicHeaders_h
+#define PublicHeaders_h
+
+#define PRODUCTIONSERVERADDRESS                 @"185.105.186.68"
+#define DEVELOPMENTSERVERADDRESS                @"149.202.30.89"
+
+#endif
